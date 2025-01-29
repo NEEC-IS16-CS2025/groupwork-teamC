087164cfd2df893243cd8bcc5006942178549f09
Awazon <61077801+Awazon@users.noreply.github.com>
2025-01-29 15:10:26 +0900

    ログイン画面にcssを適用

--- index.php
+++ index.php
@@ -27,6 +27,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
     <title>ログイン</title>
 </head>
 
